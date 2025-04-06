44af8534307e7ec6028d48b81c5b7ab6a35fbbdb
Gabrielle <chicklightning@users.noreply.github.com>
2025-04-06 15:38:05 -0700

    add-cards now fully works
    Adding a card multiple times updates the join table record count, replies change based on success or failure of adding cards, fixed some async stuff; can be called from anywhere and response is ephemeral

--- commands/slash-commands/add-cards.js
+++ commands/slash-commands/add-cards.js
@@ -1,12 +1,10 @@
-import { InteractionContextType, SlashCommandBuilder } from 'discord.js';
-import user from '../../database/models/user.js';
+import { MessageFlags, SlashCommandBuilder } from 'discord.js';
 import utilities from '../command-utilities.js';
 
 const command = {
 	data: new SlashCommandBuilder()
 		.setName('add-cards')
 		.setDescription('Add one or more cards to the list of cards you want others to trade to you.')
-        .setContexts(InteractionContextType.BotDM)
 		.addStringOption(option =>
 			option.setName('first-card')
 				.setDescription('Name of the card you want to add to your list of desired cards.')
@@ -69,7 +67,7 @@ const command = {
 		);
 	},
 	async execute(interaction) {
-		let currentUser = utilities.getUser(interaction.client, interaction.user.id, interaction.user.username);
+		let currentUser = await utilities.getUser(interaction.client, interaction.user.id, interaction.user.username);
 
 		const cardIds = [
 			interaction.options.getString('first-card'),
@@ -90,10 +88,49 @@ const command = {
 			return interaction.reply({ content: 'You must specify at least one card to add to your desired cards list.', ephemeral: true });
 		}
 
+		const cards = utilities.getModel(interaction.client.db, 'Card');
+		let addedCardCount = 0, notAddedCount = 0;
+		const addCardPromises = cardIds.map(async cardId => {
+			const card = await cards.findByPk(cardId);
+			if (card) {
+				// Check if the card already exists in the user's desiredCards
+				const existingCard = await currentUser.getDesiredCards({
+					where: { id: card.id },
+				});
+		
+				if (existingCard.length > 0) {
+					// If the card exists, increment the count
+					const userCard = existingCard[0];
+					userCard.UserCard.card_count += 1;
+					await userCard.UserCard.save();
+					addedCardCount++;
+					console.log(`[LOG] Incremented count for card ${card.id} in user ${currentUser.nickname} (${currentUser.id}).`);
+				}
+				else {
+					// If the card doesn't exist, add it to the desiredCards
+					await currentUser.addDesiredCard(card);
+					addedCardCount++;
+					console.log(`[LOG] Successfully added card ${card.id} to user ${currentUser.nickname} (${currentUser.id}).`);
+				}
+			} else {
+				notAddedCount++;
+			}
+		});
+		
+		// Wait for all card additions to complete
+		await Promise.all(addCardPromises);
 
+		currentUser.save();
+		let replyStatement = (addedCardCount > 0) ?
+			`Successfully added ${addedCardCount} card${addedCardCount === 1 ? '' : 's'} to your desired cards list!` :
+			'Sorry, no cards were added.';
+		replyStatement = (notAddedCount > 0)
+			? `${replyStatement} Something went wrong adding ${notAddedCount} card${notAddedCount === 1 ? '' : 's'}.`
+			: replyStatement;
 
 		return interaction.reply({
-			content: `Successfully added ${cardIds.length} card${cardIds.length > 1 ? 's' : ''} to your desired cards list!`,
+			content: replyStatement,
+			flags: MessageFlags.Ephemeral,
 		});
 	},
 	cooldown: 1,
